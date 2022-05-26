98e2264c51751a48ab96a5e0776209e1cc3aed66
ulvia-10 <ulvia.yulianti@gmail.com>
2022-05-26 17:51:29 +0700

    Add result to do

--- index.js
+++ index.js
@@ -1,20 +1,75 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const favorFoodMonica = new Set ([
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Eggy",
+    "Tempura",
+    "Bento",
+    "Eggy",
+    "Padang",
+    "Tteok",
+    "Sushi",
+    "Sushi",
+])
+
+const favorFoodWendy = new Set ([
+    "Tempura",
+    "Bento",
+    "Sushi",
+    "Pancake",
+    "Padang",
+    "Katsu",
+    "Geprek",
+    "Pancake",
+    "Eggy",
+])
+
+const eduMonica = new Map([
+    { name: "SD 01", city: "Jakarta", graduate: 2016 },
+    { name: "SMP 02", city: "Jakarta", graduate: 2019 },
+    { name: "SMA 03", city: "Tangerang" },
+])
+const firstUser = {
+  name: "Monica",
+  gender: "Female",
+  age: 17,
+  email: "monica@dingdong.com",
+  favoriteColor: ["Yellow", "Pink", "White", "Purple"],
+  isHavePet: true,
+  education: [eduMonica],
+  favoriteRestaurant: favorFoodMonica
+};
+const secondUser = {
+  name: "Wendy",
+  gender: "Male",
+  age: 23,
+  email: "wendy@dingdong.com",
+  favoriteColor: ["Blue", "Black", "Grey"],
+  isHavePet: false,
+  education: [
+    { name: "SD 02", city: "Jakarta", graduate: 2010 },
+    { name: "SMP 03", city: "Bogor", graduate: 2013 },
+    { name: "SMA 01", city: "Surabaya", graduate: 2016 },
+    { name: "Universitas Maju", city: "Tangerang" },
+  ],
+  favoriteRestaurant: favorFoodWendy
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser, secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
-    console.log(users.length || users.size);
-    console.log(users);
+  console.log(users.length || users.size);
+  console.log(users);
 }
 
 main();
 
 module.exports = {
-    users
+  users,
 };
